d86806a1f470a7ccf7a4b46e72c876c36086bf45
Oscar Sierra <oscarguillermosierralozano@gmail.com>
2025-04-29 12:03:32 -0500

    HU-07: Login

---
+++ backend/Backend-CaffeNet/src/main/java/com/corhuila/Backend_CaffeNet/modules/user/requests/LoginRequest.java
@@ -0,0 +1,33 @@
+package com.corhuila.Backend_CaffeNet.modules.user.requests;
+
+public class LoginRequest {
+
+    public LoginRequest(){
+
+    }
+
+    public LoginRequest(String userId, String password){
+        super();
+        this.userId = userId;
+        this.password = password;
+    }
+
+    private String userId;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
